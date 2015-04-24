e30046acc78dea22e6765caed1dc97148c32d4ff
Alex Hart <alex.hart@redspace.com>
2015-04-24 16:32:45 -0300

    Begin custom view impl

---
+++ src/com/exallium/AndroidForms/samples/LoginView.java
@@ -0,0 +1,22 @@
+package com.exallium.AndroidForms.samples;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+public class LoginView extends LinearLayout {
+
+
+    public LoginView(Context context) {
+        super(context);
+    }
+
+    public LoginView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public LoginView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+}
